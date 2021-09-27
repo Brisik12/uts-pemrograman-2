ba5449587ad593a984bd1979a2afc8ec9377d724
Rendi Sabillillah <rsabilillah12@gmail.com>
2021-09-27 17:04:17 +0700

    UTS pemrograman 2

--- products/Product.java
+++ products/Product.java
@@ -3,16 +3,19 @@ package products;
 public class Product {
     String name;
     double price;
+
     private double subtotal, discount_amount, tax_amount, grandTotal;
+    
     public Product(String name){
         this.name = name;
     }
 
     public void setPrice(double price){
         // set price of product
+        this.price = price;
     }
+
     public void getProductListing(int qty, double tax, double discount){
-        
         // contoh:
         //     qty = 4
         //     price = 100.000
@@ -25,11 +28,18 @@ public class Product {
 
         //     grandTotal = subtotal - discount + tax
 
+        setPrice(20000);
+
+        this.subtotal = qty * this.price;
+        this.discount_amount = (discount/100) * this.subtotal;
+        this.tax_amount = (tax/100) * (this.subtotal - this.discount_amount);
+        this.grandTotal = this.subtotal - this.discount_amount + this.tax_amount;
+
         System.out.println("==========ProductListing========");
         System.out.println(String.format("Harga dalam Rupiah: Rp%,.2f", price));
         System.out.println(String.format("Subtotal: Rp%,.2f", subtotal));
         System.out.println(String.format("potongan: Rp%,.2f", discount_amount));
-        System.out.println(String.format("pajak: Rp%,.2f", tax_amount));
+        System.out.println(String.format("pajak: Rp%,.2f", this.tax_amount));
         System.out.println(String.format("grand total: Rp%,.2f", grandTotal));
         System.out.println("==================");
     }
